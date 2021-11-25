cf8473e0e732288e1866ab0725ea9a545a1a2ae0
KereBere <alsydm091@gmail.com>
2021-11-25 17:39:16 +0300

    Static files added

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function () {
-  console.log('hhe');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware 1. Log');
+  next();
 };
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware 2. Log');
+  next();
+};
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
