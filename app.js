e2202d6c98f4e7a45ee315483e8fd3674479e340
KereBere <alsydm091@gmail.com>
2021-11-29 13:55:08 +0300

    MVC & CRUD added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+var methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
+// * CONNECT DB
 mongoose.connect('mongodb://localhost/pcat-test-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,27 +21,23 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/add');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePtoho);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
 });
- 
